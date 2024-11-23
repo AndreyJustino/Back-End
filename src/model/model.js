a2dc8cb1514cf56c66d4375094dc92c866340897
Junior Reis <gilmaik2010@gmail.com>
2024-11-23 12:07:10 -0300

    chore: delete unnecessary file 🗑️

--- src/model/model.js
+++
@@ -1,9 +0,0 @@
-import { DataTypes } from "sequelize";
-import { database } from "../database/config.js";
-
-const User = database.define('User', {
-  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nome: { type: DataTypes.STRING, allowNull: false }
-});
-
-export { User };
--- src/routes/router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { listUsers } from "../controller/index.js";
-
-const userRouter = express.Router()
-
-userRouter.get('/users', listUsers);
-
-export { userRouter };
