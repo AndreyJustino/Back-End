9de0c173dbf0f412c73768b679bbfc306fc17ccb
Junior Reis <gilmaik2010@gmail.com>
2024-11-23 16:08:49 -0300

    feat: add token verification functionality 🔐

--- src/routes/Hotel.routes.js
+++ src/routes/Hotel.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createHotels, listHotels } from "../controller/hotels/hotel.controller.js";
+import verifyToken from "../middleware/verifyToken.js"
 
 const hotelRouter = express.Router()
 
-hotelRouter.get('/hotels', listHotels);
+hotelRouter.get('/hotels', verifyToken, listHotels);
 hotelRouter.post('/hotels', createHotels);
 
 
