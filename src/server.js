8338b3ac2ee0edde209329d9186f11bf6b342052
Junior Reis <gilmaik2010@gmail.com>
2024-11-23 12:07:46 -0300

    feat: add hotel routes 🏨

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import corsMiddleware from './middleware/corsMiddleware.js';
 import { tryConnectSequelize } from './database/config.js';
-import { userRouter } from './routes/router.js';
-
+import { hotelRouter } from './routes/Hotel.routes.js';
 const port = process.env.PORT || 4000;
 const server = express();
 
 server.use(express.json());
 server.use(corsMiddleware);
-server.use(userRouter)
+server.use(hotelRouter)
 
 
 server.listen(port, () => {
