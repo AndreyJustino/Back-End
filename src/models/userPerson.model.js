7df17d647db7b9132a9378742d7d843b53a58a78
Junior Reis <gilmaik2010@gmail.com>
2024-11-23 17:21:09 -0300

    feat: add company and person models 🏢👤

---
+++ src/models/userPerson.model.js
@@ -0,0 +1,31 @@
+import { DataTypes } from "sequelize";
+import { database } from "../database/config.js";
+
+export const Person = database.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  age: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  phoneNumber: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
